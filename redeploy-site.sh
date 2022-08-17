db19d27788b394f12d28eb7be1d6971e65d2b1ec
Katherine <kad.delgado@gmail.com>
2022-08-17 07:03:05 +0000

    Fixed Dockerfile to python, deleted virtuan env in redeploy-site

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -2,9 +2,6 @@
 
 cd /root/pe-portfolio-site-troubleshoot-kanaild
 git fetch && git reset origin/main --hard
-python -m venv python3-virtualenv
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
 
 docker compose down
 docker compose up -d --build
